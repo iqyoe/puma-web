0d92d3359a24899f4ef12c37da5865e69e895bc7
Ikyu <muhiqballukman@gmail.com>
2018-01-04 19:55:56 +0700

    minified
    minified css and js for back-end purposes

---
+++ assets/js/event.js
@@ -0,0 +1,72 @@
+    $('#myCarousel').carousel({
+        interval: 0
+    });
+
+    $(document).ready(function(){
+
+        var showHeaderAt = 100;
+
+        var win = $(window),
+                body = $('body');
+
+        // Show the fixed header only on larger screen devices
+
+        if(win.width() > 600){
+
+            // When we scroll more than 150px down, we set the
+            // "fixed" class on the body element.
+
+            win.on('scroll', function(e){
+
+                if(win.scrollTop() > showHeaderAt) {
+                    body.addClass('fixed');
+                    $('.header-user-menu').hide();
+                    $('#headlogo').show();
+                }
+                else {
+                    body.removeClass('fixed');
+                    $('.header-user-menu').show();
+                    $('#headlogo').hide();
+                }
+            });
+
+        }
+
+    });
+
+    $(document).ready(function(){
+
+        var userMenu = $('.header-user-dropdown .header-user-menu');
+
+        userMenu.on('touchend', function(e){
+
+            userMenu.addClass('show');
+
+            e.preventDefault();
+            e.stopPropagation();
+
+        });
+
+        // This code makes the user dropdown work on mobile devices
+
+        $(document).on('touchend', function(e){
+
+            // If the page is touched anywhere outside the user menu, close it
+            userMenu.removeClass('show');
+
+        });
+
+    });
+
+    $( document ).ready(function() {
+       // $("[rel='tooltip']").tooltip();
+
+        $('.event-list .thumbnail, .news .thumbnail').hover(
+            function(){
+                $(this).find('.caption-show').slideDown(250); //.fadeIn(250)
+            },
+            function(){
+                $(this).find('.caption-show').slideUp(250); //.fadeOut(205)
+            }
+        );
+    });
